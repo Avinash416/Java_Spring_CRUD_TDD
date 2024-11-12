3ec9358766217019db93463694d671b60b78cc7f
AvinashJadhav112 <avinash.j@vervetronics.com>
2024-11-12 14:12:21 +0530

    feat: Add mysql dependency and make changes in model accordingly

--- src/main/java/com/ecom/server/Model/ProductModel.java
+++ src/main/java/com/ecom/server/Model/ProductModel.java
@@ -1,19 +1,23 @@
 package com.ecom.server.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "my_products")
 public class ProductModel {
     @Id
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
+
     public String title;
     public double price;
     public String description;
